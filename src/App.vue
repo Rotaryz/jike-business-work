6f559e990084454bb5acc80de482065d532cf591
panranran <ran@jkweixin.com>
2018-07-07 16:16:42 +0800

    组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view/>
-    <slide-view v-for="(item, index) in list" :key="item.id" :styles="item.style" :index="index" :item="item" v-on:touchBegin="touchBegin" v-on:touchEnd="touchEnd" v-on:touchMove="touchMove" v-on:del="del"></slide-view>
+    <slide-view v-for="(item, index) in list" :key="item.id" :showIdx.sync="showIdx" :index="index" :ref="'item' + index" v-on:touchBegin="touchBegin" v-on:del="del" v-on:grouping="grouping"></slide-view>
   </div>
 </template>
 
@@ -12,25 +12,23 @@ export default {
   name: 'App',
   data() {
     return {
-      startX: '',
-      endX: '',
-      moveStart: '',
+      showIdx: -1,
       list: [
-        {id: 1, style: 'width: 0'},
-        {id: 2, style: 'width: 0'},
-        {id: 3, style: 'width: 0'},
-        {id: 4, style: 'width: 0'},
-        {id: 5, style: 'width: 0'},
-        {id: 6, style: 'width: 0'},
-        {id: 7, style: 'width: 0'},
-        {id: 8, style: 'width: 0'},
-        {id: 9, style: 'width: 0'},
-        {id: 10, style: 'width: 0'},
-        {id: 11, style: 'width: 0'},
-        {id: 12, style: 'width: 0'},
-        {id: 13, style: 'width: 0'},
-        {id: 14, style: 'width: 0'},
-        {id: 15, style: 'width: 0'}
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4},
+        {id: 5},
+        {id: 6},
+        {id: 7},
+        {id: 8},
+        {id: 9},
+        {id: 10},
+        {id: 11},
+        {id: 12},
+        {id: 13},
+        {id: 14},
+        {id: 15}
       ]
     }
   },
@@ -43,67 +41,19 @@ export default {
   methods: {
     ...mapGetters(['argument', 'argument1']),
     ...mapActions(['test', 'test1']),
-    touchBegin(e, index, item) {
-      this.startX = e.touches[0].clientX
-      this.moveStart = this.startX
-      this.list = this._initList(this.list, index)
-    },
-    touchEnd(e, index, item) {
-      this.endX = e.changedTouches[0].clientX
-      let differ, res
-      if (item.show) {
-        differ = this.endX - this.startX
-        if (differ > 0) {
-          res = Object.assign({}, item, {style: 'width: 0px; transition: all .3s', show: false})
-        } else {
-          res = Object.assign({}, item, {style: 'width: 60px; transition: all .3s', show: false})
-        }
-      } else {
-        differ = (this.startX - this.endX) / 2
-        if (differ <= 30) {
-          res = Object.assign({}, item, {style: 'width: 0px; transition: all .3s', show: false})
-        } else {
-          res = Object.assign({}, item, {style: 'width: 60px; transition: all .3s', show: true})
-        }
+    touchBegin(index) {
+      let name = 'item' + this.showIdx
+      if (this.showIdx !== -1) {
+        this.$refs[name][0]._itemInit()
       }
-      this.$set(this.list, index, res)
+      this.showIdx = index
     },
-    touchMove(e, index, item) {
-      let moveEnd = e.changedTouches[0].clientX
-      let differ, width, endWidth, beginWidth
-      if (item.show) {
-        differ = (moveEnd - this.moveStart) * 2
-        width = 60 - differ
-        endWidth = 0
-        beginWidth = 60
-      } else {
-        differ = (this.moveStart - moveEnd) / 2
-        width = differ
-        endWidth = 60
-        beginWidth = 0
-      }
-      let res
-      if (differ < 0) {
-        res = Object.assign({}, item, {style: `width: ${beginWidth}px`})
-        this.moveStart = moveEnd
-      } else if (differ > 60) {
-        res = Object.assign({}, item, {style: `width: ${endWidth}px`})
-      } else {
-        res = Object.assign({}, item, {style: `width: ${width}px`})
-      }
-      this.$set(this.list, index, res)
+    del(idx) {
+      console.log(idx)
+      this.list.splice(idx, 1)
     },
-    _initList(arr, idx) {
-      let resArr = arr.map((item, index) => {
-        if (index !== idx) {
-          item.style = 'width: 0px; transition: all .3s'
-        }
-        return item
-      })
-      return resArr
-    },
-    del(item, idx) {
-      console.log(item, idx)
+    grouping(idx) {
+      console.log(idx)
     }
   },
   components: {
