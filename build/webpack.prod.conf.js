e49c175fb741a1751ab98a1538449832dabb6495
Ben <ben@jkweixin.com>
2018-07-11 14:52:17 +0800

    test oauth

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,7 +11,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'test'
+const env = process.env.BUILD_ENV === 'test'
   ? require('../config/test.env')
   : require('../config/prod.env')
 
