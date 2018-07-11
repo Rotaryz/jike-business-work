e040053b0a48a91c6b8e495eb21ff6add1bdfae5
Ben <ben@jkweixin.com>
2018-07-11 15:18:19 +0800

    test oauth

--- src/common/js/oauth.js
+++ src/common/js/oauth.js
@@ -1,6 +1,6 @@
 export default class Oauth {
   constructor() {
-    const uri = 'http://business-work.jkweixin.com/oauth'
+    const uri = 'http://business-work.jkweixin.net/oauth'
     const redirectUri = encodeURIComponent(uri)
     this.oauthUri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=ww8254a365bf92e5aa&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
   }
