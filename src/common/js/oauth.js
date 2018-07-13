a59139c9e208242733d748c8a0df8db1df456fe2
Ben <ben@jkweixin.com>
2018-07-13 17:56:59 +0800

    appId config

--- src/common/js/oauth.js
+++ src/common/js/oauth.js
@@ -1,7 +1,8 @@
 export default class Oauth {
   constructor() {
+    const appId = 'wwaa9846f58158d92e'
     const uri = location.origin + '/oauth'
     const redirectUri = encodeURIComponent(uri)
-    this.oauthUri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=ww80c31779e77cc4ba&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
+    this.oauthUri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
   }
 }
