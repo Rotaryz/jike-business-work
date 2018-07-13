efb4d6563ff467e6992e8145876956bab7d31287
panranran <ran@jkweixin.com>
2018-07-13 17:54:24 +0800

    im优化

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -5,11 +5,11 @@ const DEV_URL = {
 }
 
 const TEST_URL = {
-  api: 'http://business-api.jerryf.cn'
+  api: 'http://business-api.jerryf.net'
 }
 
 const PROD_URL = {
-  api: 'http://business-api.jerryf.cn'
+  api: 'http://business-api.jerryf.com'
 }
 
 export const BASE_URL = env.NODE_ENV === 'production' ? PROD_URL : env.NODE_ENV === 'test' ? TEST_URL : DEV_URL
