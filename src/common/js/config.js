90676c8a799b02736d5a5f747bd068fe9a047465
Ben <ben@jkweixin.com>
2018-07-14 11:53:07 +0800

    切换为测试环境

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -9,8 +9,7 @@ const DEV_URL = {
 }
 
 const TEST_URL = {
-  // api: 'https://business-api.jkweixin.net'
-  api: 'https://business-api.jerryf.cn'
+  api: 'https://business-api.jkweixin.net'
 }
 
 const PROD_URL = {
