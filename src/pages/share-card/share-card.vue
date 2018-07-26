612bf8f70ba73da75b6d1fa35aef53533f5f0b6a
周旋 <zhouxuan@jkweixin.com>
2018-07-26 18:36:20 +0800

    优化图片上传

--- src/pages/share-card/share-card.vue
+++ src/pages/share-card/share-card.vue
@@ -2,33 +2,41 @@
   <transition :name="slide">
 
     <div class="share-card">
-      <!--<img style="height: 100vh;width: 100vw;opacity: 0" :src="card.avatar">-->
-      <img :src="card.avatar" alt="" class="qr-code">
-
-      <!--<div class="share-box">-->
-        <!--<div class="share-con">-->
-          <!--<img class="share-top" :src="card.avatar" alt="">-->
-          <!--<div class="share-bottom">-->
-            <!--<div class="name-profession">-->
-              <!--<div class="name">{{card.name}}</div>-->
-              <!--<div class="line" v-if="showPosition"></div>-->
-              <!--<div class="name-profession">{{card.position}}</div>-->
-            <!--</div>-->
-            <!--<div class="buss-name">{{card.department}}</div>-->
-            <!--<div class="code-padding"></div>-->
-            <!--<div class="code-phone" v-if="showMobile">-->
-              <!--<img src="./icon-telephone_ash@2x.png" alt="" class="img-phone">-->
-              <!--<div class="text">{{card.business_card_mobile}}</div>-->
-              <!--<img :src="card.qrcode" alt="" class="share-code">-->
-            <!--</div>-->
-          <!--</div>-->
-          <!--&lt;!&ndash;<p class="peo-name">{{card.name}}</p>&ndash;&gt;-->
-          <!--&lt;!&ndash;<p class="peo-position">{{card.position}}</p>&ndash;&gt;-->
-          <!--&lt;!&ndash;<p class="buss-name">{{card.department}}</p>&ndash;&gt;-->
-          <!--&lt;!&ndash;<img class="qr-code" :src="card.qrcode" alt="">&ndash;&gt;-->
+      <!--<img style="height: 100vhwidth: 100vwopacity: 0" :src="card.avatar">-->
+      <div class="qrcode_box">
+        <img :src="card.qrcode">
+        <!--<div v-show="card.qrcode === ''" >-->
+        <!--< img src="../../assets/imgs/refresh_code.png" alt="" style="width: 1.7remheight: 1.7remmargin-top: 1.3rem">-->
         <!--</div>-->
-      <!--</div>-->
+      </div>
+      <p v-show="card.qrcode !== ''">长按查看小程序名片</p>
+      <p v-show="card.qrcode === ''">小程序二维码获取失败,点击重新获取</p>
     </div>
+
+    <!--<div class="share-box">-->
+    <!--<div class="share-con">-->
+    <!--<img class="share-top" :src="card.avatar" alt="">-->
+    <!--<div class="share-bottom">-->
+    <!--<div class="name-profession">-->
+    <!--<div class="name">{{card.name}}</div>-->
+    <!--<div class="line" v-if="showPosition"></div>-->
+    <!--<div class="name-profession">{{card.position}}</div>-->
+    <!--</div>-->
+    <!--<div class="buss-name">{{card.department}}</div>-->
+    <!--<div class="code-padding"></div>-->
+    <!--<div class="code-phone" v-if="showMobile">-->
+    <!--<img src="./icon-telephone_ash@2x.png" alt="" class="img-phone">-->
+    <!--<div class="text">{{card.business_card_mobile}}</div>-->
+    <!--<img :src="card.qrcode" alt="" class="share-code">-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--&lt!&ndash<p class="peo-name">{{card.name}}</p>&ndash&gt-->
+    <!--&lt!&ndash<p class="peo-position">{{card.position}}</p>&ndash&gt-->
+    <!--&lt!&ndash<p class="buss-name">{{card.department}}</p>&ndash&gt-->
+    <!--&lt!&ndash<img class="qr-code" :src="card.qrcode" alt="">&ndash&gt-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--</div>-->
   </transition>
 </template>
 
@@ -132,4 +140,39 @@
 
   .qr-code
     width: 70vw
+
+  .qrcode_box
+    padding-top: 30px
+    text-align: center
+
+  .qrcode_box img, .qrcode_box div
+    width: 4.3rem
+    height: 4.3rem
+    margin-top: 0.53rem
+
+  .qrcode_box div
+    display: inline-block
+    text-align: center
+
+  .qrcode_back_img /* background-image: url("../../assets/imgs/card_background.png")
+    background-size: cover */
+    background-color: #fff
+
+  .qrcode_box p
+    font-size: 0.26rem
+    color: #8B8E92
+    text-align: center
+    margin-top: 0.3rem
+    margin-bottom: 0.8rem
+
+  @keyframes rotate
+    from
+      transform: rotate(0deg)
+
+    to
+      transform: rotate(360deg)
+
+  .code_rotate
+    animation: rotate 2s infinite
+
 </style>
