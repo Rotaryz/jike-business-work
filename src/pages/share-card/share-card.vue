4a850dc972264ccc6ec6eac69856b69b7023f860
周旋 <zhouxuan@jkweixin.com>
2018-07-26 19:06:13 +0800

    优化图片上传

--- src/pages/share-card/share-card.vue
+++ src/pages/share-card/share-card.vue
@@ -1,43 +1,34 @@
 <template>
-  <!--<transition :name="slide">-->
+  <transition :name="slide">
 
     <div class="share-card">
-      <!--<img style="height: 100vhwidth: 100vwopacity: 0" :src="card.avatar">-->
-      <div class="qrcode_box">
-        <img :src="card.avatar">
-        <!--<div v-show="card.qrcode === ''" >-->
-        <!--< img src="../../assets/imgs/refresh_code.png" alt="" style="width: 1.7remheight: 1.7remmargin-top: 1.3rem">-->
-        <!--</div>-->
+      <div class="share-box">
+        <div class="share-con">
+          <img class="share-top" :src="card.avatar" alt="">
+          <div class="share-bottom">
+            <div class="name-profession">
+              <div class="name">{{card.name}}</div>
+              <div class="line" v-if="showPosition"></div>
+              <div class="name-profession">{{card.position}}</div>
+            </div>
+            <div class="buss-name">{{card.department}}</div>
+            <div class="code-padding"></div>
+            <div class="code-phone" v-if="showMobile">
+              <div class="code-phone-box">
+                <img src="./icon-telephone_ash@2x.png" alt="" class="img-phone">
+                <div class="text">{{card.business_card_mobile}}</div>
+              </div>
+              <img :src="card.qrcode" alt="" class="share-code">
+            </div>
+          </div>
+          <!--<p class="peo-name">{{card.name}}</p>-->
+          <!--<p class="peo-position">{{card.position}}</p>-->
+          <!--<p class="buss-name">{{card.department}}</p>-->
+          <!--<img class="qr-code" :src="card.qrcode" alt="">-->
+        </div>
       </div>
-      <p v-show="card.qrcode !== ''">长按查看小程序名片</p>
-      <p v-show="card.qrcode === ''">小程序二维码获取失败,点击重新获取</p>
     </div>
-
-    <!--<div class="share-box">-->
-    <!--<div class="share-con">-->
-    <!--<img class="share-top" :src="card.avatar" alt="">-->
-    <!--<div class="share-bottom">-->
-    <!--<div class="name-profession">-->
-    <!--<div class="name">{{card.name}}</div>-->
-    <!--<div class="line" v-if="showPosition"></div>-->
-    <!--<div class="name-profession">{{card.position}}</div>-->
-    <!--</div>-->
-    <!--<div class="buss-name">{{card.department}}</div>-->
-    <!--<div class="code-padding"></div>-->
-    <!--<div class="code-phone" v-if="showMobile">-->
-    <!--<img src="./icon-telephone_ash@2x.png" alt="" class="img-phone">-->
-    <!--<div class="text">{{card.business_card_mobile}}</div>-->
-    <!--<img :src="card.qrcode" alt="" class="share-code">-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--&lt!&ndash<p class="peo-name">{{card.name}}</p>&ndash&gt-->
-    <!--&lt!&ndash<p class="peo-position">{{card.position}}</p>&ndash&gt-->
-    <!--&lt!&ndash<p class="buss-name">{{card.department}}</p>&ndash&gt-->
-    <!--&lt!&ndash<img class="qr-code" :src="card.qrcode" alt="">&ndash&gt-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
-  <!--</transition>-->
+  </transition>
 </template>
 
 <script>
@@ -81,14 +72,17 @@
   @import "~common/stylus/variable"
   @import '~common/stylus/mixin'
   .share-card
+    position: relative
+    z-index: 100
     height: 100vh
-    background: $color-white
+    padding-top: 20px
+    box-sizing: border-box
+    background: $color-background
     .share-box
       display: flex
       flex-direction: column
       width: 305px
       margin: 0 auto
-      padding-top: 20px
     .share-con
       background: $color-white-fff
       border-radius: 2px
@@ -124,54 +118,29 @@
           color: $color-20202E
           margin-top: 15px
         .code-padding
-          padding-bottom: 147px
+          padding-bottom: 31px
           width: 100%
         .code-phone
           layout(row)
+          display :flex
+          height: 74px
+          align-items :flex-end
+          justify-content :space-between
+          .code-phone-box
+            display :flex
           .img-phone
             display: block
             width: 14px
             height: 14px
             margin-right: 5px
           .text
+            height: $font-size-medium
             font-family: $font-family-regular
             font-size: $font-size-medium
             color: $color-text-88
 
-  .qr-code
-    width: 70vw
-
-  .qrcode_box
-    padding-top: 30px
-    text-align: center
-    img
-      width: 4.3rem
-      height: 4.3rem
-      margin-top: 0.53rem
-
-  .qrcode_box div
-    display: inline-block
-    text-align: center
-
-  .qrcode_back_img /* background-image: url("../../assets/imgs/card_background.png")
-    background-size: cover */
-    background-color: #fff
-
-  .qrcode_box p
-    font-size: 0.26rem
-    color: #8B8E92
-    text-align: center
-    margin-top: 0.3rem
-    margin-bottom: 0.8rem
-
-  @keyframes rotate
-    from
-      transform: rotate(0deg)
-
-    to
-      transform: rotate(360deg)
-
-  .code_rotate
-    animation: rotate 2s infinite
-
+  .share-code
+    display :block
+    width: 74px
+    height: 74px
 </style>
