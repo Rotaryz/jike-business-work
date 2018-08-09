1c92a31a11b93a8b19706e65db23128185d411ac
panranran <ran@jkweixin.com>
2018-08-09 12:27:51 +0800

    最近联系人接口调整

--- src/api/im.js
+++ src/api/im.js
@@ -37,7 +37,7 @@ export default {
 
   // 最近联系人列表
   getContactList(data, loading = true) {
-    const url = `/api/employee/ws-customers-recent-contacts`
+    const url = `/api/employee/customers-recent-contacts`
     return request.post(url, data, loading)
   }
 }
