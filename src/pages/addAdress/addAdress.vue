d95a781c395c0337de8baeb0d184baecb061171b
wengtianliang <wengtianliang@jkweixin.com>
2018-09-01 18:14:54 +0800

    change Address

--- src/pages/addAdress/addAdress.vue
+++ src/pages/addAdress/addAdress.vue
@@ -1,129 +1,29 @@
 <!--<template>-->
   <!--<div class="change-autograph">-->
     <!--<scroll ref="scroll">-->
-      <!--<div class="address-box">-->
-        <!--<div class="address-list" @click="selcetAddress">-->
-          <!--<div class="text">所在地区</div>-->
-          <!--<div class="text-right" v-if="address.length === 0">请选择地区</div>-->
-          <!--<div class="text-right-acitive" v-if="address.length !== 0">{{address}}</div>-->
-          <!--<img src="./icon-pressed@2x.png" alt="" class="address-img">-->
-        <!--</div>-->
-        <!--<div class="address-list">-->
-          <!--<div class="text">详细地址</div>-->
-          <!--<input type="text" class="item-input" v-model="detailAdress" placeholder="请输入详细地址"/>-->
-        <!--</div>-->
-      <!--</div>-->
     <!--</scroll>-->
     <!--<div class="btn">-->
       <!--<div class="btn-item btn-green" @click="saveAdress">确定</div>-->
     <!--</div>-->
     <!--<toast ref="toast"></toast>-->
-    <!--<awesome-picker-->
-      <!--ref="picker"-->
-      <!--:data="cityData"-->
-      <!--@cancel="handlePickerCancel"-->
-      <!--@confirm="handlePickerConfirm">-->
-    <!--</awesome-picker>-->
   <!--</div>-->
 <!--</template>-->
 
 
 <!--<script type="text/ecmascript-6">-->
-  <!--import Scroll from 'components/scroll/scroll'-->
-  <!--import {mapGetters} from 'vuex'-->
-  <!--import {cityData} from 'common/js/utils'-->
-  <!--import Toast from 'components/toast/toast'-->
-  <!--import {Mine} from 'api'-->
-  <!--import {ERR_OK} from '../../common/js/config'-->
-  <!--import AMap from 'AMap'-->
 
   <!--export default {-->
     <!--name: 'addAdress',-->
     <!--data() {-->
       <!--return {-->
-        <!--cityData,-->
-        <!--province: '',-->
-        <!--city: '',-->
-        <!--area: '',-->
-        <!--address: '',-->
-        <!--detailAdress: '',-->
-        <!--latitude: 0,-->
-        <!--longitude: 0-->
       <!--}-->
     <!--},-->
     <!--created() {-->
-      <!--Mine.getMyInfoAddress().then((res) => {-->
-        <!--if (res.error === ERR_OK) {-->
-          <!--this.province = res.message.province-->
-          <!--this.city = res.message.city-->
-          <!--this.area = res.message.area-->
-          <!--this.detailAdress = res.message.address-->
-          <!--this.address = res.message.province + res.message.city + res.message.area-->
-        <!--} else {-->
-          <!--this.$refs.toast.show(res.message)-->
-        <!--}-->
-      <!--})-->
     <!--},-->
     <!--mounted() {-->
     <!--},-->
     <!--methods: {-->
-      <!--handlePickerCancel(e) {-->
-        <!--console.log(e)-->
-      <!--},-->
-      <!--handlePickerConfirm(e) {-->
-        <!--let text = ''-->
-        <!--for (var i = 0; i < e.length; i++) {-->
-          <!--text += e[i].value-->
-        <!--}-->
-        <!--this.province = e[0].value-->
-        <!--this.city = e[1].value-->
-        <!--this.area = e[2].value-->
-        <!--this.address = text-->
-      <!--},-->
-      <!--selcetAddress() {-->
-        <!--this.$refs.picker.show()-->
-      <!--},-->
-      <!--saveAdress() {-->
-        <!--if (this.address.length === 0) {-->
-          <!--this.$refs.toast.show('请选择所在地区')-->
-          <!--return-->
-        <!--}-->
-        <!--if (this.detailAdress.length === 0) {-->
-          <!--this.$refs.toast.show('请输入详细地址')-->
-          <!--return-->
-        <!--}-->
-        <!--let text = this.address + this.detailAdress-->
-        <!--this.getGeocoder(text)-->
-      <!--},-->
-      <!--getGeocoder(text) {-->
-        <!--let that = this-->
-        <!--let geocoder-->
-        <!--AMap.plugin('AMap.Geocoder', function () {-->
-          <!--geocoder = new AMap.Geocoder()-->
-        <!--})-->
-        <!--geocoder.getLocation(text, function (status, result) {-->
-          <!--if (status === 'complete' && result.info === 'OK') {-->
-            <!--that.longitude = result.geocodes[0].location.lng-->
-            <!--that.latitude = result.geocodes[0].location.lat-->
-            <!--let data = {-->
-              <!--address: that.detailAdress,-->
-              <!--province: that.province,-->
-              <!--city: that.city,-->
-              <!--area: that.area,-->
-              <!--longitude: that.longitude,-->
-              <!--latitude: that.latitude-->
-            <!--}-->
-            <!--Mine.updateMyInfoAddress(data).then((res) => {-->
-              <!--if (res.error === ERR_OK) {-->
-                <!--that.$router.back()-->
-                <!--that.$emit('getSign')-->
-              <!--} else {-->
-                <!--that.$refs.toast.show(res.message)-->
-              <!--}-->
-            <!--})-->
-          <!--}-->
-        <!--})-->
-      <!--}-->
+      <!---->
     <!--},-->
     <!--computed: {-->
       <!--...mapGetters(['ios']),-->
@@ -177,53 +77,7 @@
       <!--.text-light-->
         <!--color: #CCCCCC-->
 
-  <!--.address-box-->
-    <!--background: $color-white-fff-->
-    <!--padding: 0 15px-->
-    <!--.address-list-->
-      <!--height: 55px-->
-      <!--layout(row)-->
-      <!--align-items: center-->
-      <!--position: relative-->
-      <!--border-bottom-1px(#ecedf1)-->
-      <!--.text-->
-        <!--font-size: $font-size-14-->
-        <!--font-family: $font-family-regular-->
-        <!--color: $color-888888-->
-        <!--margin-right: 16px-->
-      <!--.text-right-->
-        <!--font-size: $font-size-14-->
-        <!--font-family: $font-family-regular-->
-        <!--color: $color-ccc-->
-      <!--.text-right-acitive-->
-        <!--font-size: $font-size-14-->
-        <!--font-family: $font-family-regular-->
-        <!--color: $color-text-->
-      <!--.address-img-->
-        <!--position: absolute-->
-        <!--right: 0-->
-        <!--width: 7.5px-->
-        <!--height: 11.5px-->
-        <!--top: 0-->
-        <!--bottom: 0-->
-        <!--margin: auto-->
-      <!--.item-input-->
-        <!--height: 53px-->
-        <!--flex: 1-->
-        <!--font-size: $font-size-14-->
-        <!--font-family: $font-family-regular-->
-        <!--color: $color-text-->
-        <!--padding-right: 15px-->
-        <!--border: none-->
-        <!--outline: none-->
-      <!--.item-input::-webkit-input-placeholder-->
-        <!--color: #ccc-->
-      <!--.item-input::-ms-input-placeholder-->
-        <!--color: #ccc-->
-      <!--.item-input::-moz-placeholder-->
-        <!--color: #ccc-->
-      <!--.address-list:last-child-->
-        <!--border-none()-->
+
 
   <!--.save-btn-->
     <!--background: $color-20202E-->
@@ -265,10 +119,28 @@
           <span class="text-dark">{{title.length}}</span>
           <span class="text-light">/50</span>
         </div>
+        <div class="address-box">
+          <div class="address-list" @click="selcetAddress">
+            <div class="text">所在地区</div>
+            <div class="text-right" v-if="address.length === 0">请选择地区</div>
+            <div class="text-right-acitive" v-if="address.length !== 0">{{address}}</div>
+            <img src="./icon-pressed@2x.png" alt="" class="address-img">
+          </div>
+          <div class="address-list">
+            <div class="text">详细地址</div>
+            <input type="text" class="item-input" v-model="detailAdress" placeholder="请输入详细地址"/>
+          </div>
+        </div>
       </scroll>
       <div class="btn">
-        <div class="btn-item btn-green" @click="_submit">确定</div>
+        <div class="btn-item btn-green" @click="saveAdress">确定</div>
       </div>
+      <awesome-picker
+        ref="picker"
+        :data="cityData"
+        @cancel="handlePickerCancel"
+        @confirm="handlePickerConfirm">
+      </awesome-picker>
       <toast ref="toast"></toast>
     </div>
   </transition>
@@ -276,21 +148,41 @@
 
 <script>
   import Scroll from 'components/scroll/scroll'
-  import { Business } from 'api'
+  import { Business, Mine } from 'api'
   import { ERR_OK } from 'common/js/config'
-  import Toast from 'components/toast/toast'
   import { mapActions, mapGetters } from 'vuex'
+  import {cityData} from 'common/js/utils'
+  import Toast from 'components/toast/toast'
+  import AMap from 'AMap'
 
   export default {
     name: 'change-autograph',
     data () {
       return {
-        title: ''
+        title: '',
+        cityData,
+        province: '',
+        city: '',
+        area: '',
+        address: '',
+        detailAdress: '',
+        latitude: 0,
+        longitude: 0
       }
     },
     created () {
       this.title = this.$store.state.signature
-      console.log(this.title)
+      Mine.getMyInfoAddress().then((res) => {
+        if (res.error === ERR_OK) {
+          this.province = res.message.province
+          this.city = res.message.city
+          this.area = res.message.area
+          this.detailAdress = res.message.address
+          this.address = res.message.province + res.message.city + res.message.area
+        } else {
+          this.$refs.toast.show(res.message)
+        }
+      })
     },
     methods: {
       ...mapActions(['setSignature']),
@@ -307,6 +199,63 @@
       },
       _back () {
         this.$router.back()
+      },
+      handlePickerCancel(e) {
+        console.log(e)
+      },
+      handlePickerConfirm(e) {
+        let text = ''
+        for (var i = 0; i < e.length; i++) {
+          text += e[i].value
+        }
+        this.province = e[0].value
+        this.city = e[1].value
+        this.area = e[2].value
+        this.address = text
+      },
+      selcetAddress() {
+        this.$refs.picker.show()
+      },
+      saveAdress() {
+        if (this.address.length === 0) {
+          this.$refs.toast.show('请选择所在地区')
+          return
+        }
+        if (this.detailAdress.length === 0) {
+          this.$refs.toast.show('请输入详细地址')
+          return
+        }
+        let text = this.address + this.detailAdress
+        this.getGeocoder(text)
+      },
+      getGeocoder(text) {
+        let that = this
+        let geocoder
+        AMap.plugin('AMap.Geocoder', function () {
+          geocoder = new AMap.Geocoder()
+        })
+        geocoder.getLocation(text, function (status, result) {
+          if (status === 'complete' && result.info === 'OK') {
+            that.longitude = result.geocodes[0].location.lng
+            that.latitude = result.geocodes[0].location.lat
+            let data = {
+              address: that.detailAdress,
+              province: that.province,
+              city: that.city,
+              area: that.area,
+              longitude: that.longitude,
+              latitude: that.latitude
+            }
+            Mine.updateMyInfoAddress(data).then((res) => {
+              if (res.error === ERR_OK) {
+                that.$router.back()
+                that.$emit('getSign')
+              } else {
+                that.$refs.toast.show(res.message)
+              }
+            })
+          }
+        })
       }
     },
     computed: {
@@ -360,7 +309,53 @@
         color: $color-text
       .text-light
         color: #CCCCCC
-
+  .address-box
+    background: $color-white-fff
+    padding: 0 15px
+    .address-list
+      height: 55px
+      layout(row)
+      align-items: center
+      position: relative
+      border-bottom-1px(#ecedf1)
+      .text
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-888888
+        margin-right: 16px
+      .text-right
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-ccc
+      .text-right-acitive
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-text
+      .address-img
+        position: absolute
+        right: 0
+        width: 7.5px
+        height: 11.5px
+        top: 0
+        bottom: 0
+        margin: auto
+      .item-input
+        height: 53px
+        flex: 1
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-text
+        padding-right: 15px
+        border: none
+        outline: none
+      .item-input::-webkit-input-placeholder
+        color: #ccc
+      .item-input::-ms-input-placeholder
+        color: #ccc
+      .item-input::-moz-placeholder
+        color: #ccc
+      .address-list:last-child
+        border-none()
   .btn
     position: relative
     z-index: 10
