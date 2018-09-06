a2db5fbc16926b064f70c870c8b46707a1b83d45
wengtianliang <wengtianliang@jkweixin.com>
2018-09-06 16:39:38 +0800

    cropArea

--- src/pages/mine-info/mine-info.vue
+++ src/pages/mine-info/mine-info.vue
@@ -47,7 +47,7 @@
           :background="status"
           :cropBoxResizable="status"
           :aspectRatio="1"
-          :autoCropArea="1"
+          :autoCropArea="0.8"
           :dragMode="'move'"
           :checkCrossOrigin="false"
           :cropBoxMovable="false"
